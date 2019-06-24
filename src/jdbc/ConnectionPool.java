ea042f20f5f61fbe2761c247db59db48dc9ceb29
MolyXl <345957335@qq.com>
2019-06-24 18:02:39 +0800

    数据库驱动类

--- src/jdbc/ConnectionPool.java
+++ src/jdbc/ConnectionPool.java
@@ -10,6 +10,42 @@ import java.util.List;
  * @date 2019/6/24
  */
 public class ConnectionPool {
-    private List<Connection> pool = new ArrayList<>();
+    private static List<Connection> pool = new ArrayList<>();
+    private static int Normal = 10; //初始化连接数
+    private static int MAX = 20; //最大的连接数
+    private static int MIN = 5; //最小的连接数
+
+    static {
+        while (pool.size() < 10) {
+            pool.add(JdbcConnection.getInstance());
+        }
+    }
+
+    public static Connection getConnection() {
+        if (pool.size() > MIN) {
+            return pool.remove(0);
+        } else {
+            if (pool.size() <= MAX) {
+                Connection conn = JdbcConnection.getInstance();
+                pool.add(conn);
+                return conn;
+            } else {
+                Connection conn = JdbcConnection.getInstance();
+                return conn;
+            }
+        }
+    }
+
+    public static void close(Connection conn) {
+        if (conn != null && pool.size() < MAX) {
+            pool.add(conn);
+        }else{
+            try {
+                conn.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
