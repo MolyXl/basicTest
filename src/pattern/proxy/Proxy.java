2e314e67a2e2a2a2699292cc00ab6304bec62bbf
MolyXl <345957335@qq.com>
2019-06-26 11:32:09 +0800

    设计模式,桥接模式

--- src/pattern/proxy/Proxy.java
+++ src/pattern/proxy/Proxy.java
@@ -6,25 +6,29 @@ import interfaces.impl.SubjectImpl2;
 
 /**
  * @author MaZhuli
- * @description 代理模式
+ * @description 代理模式/桥接模式
  * @date 2019/6/26
  */
 public class Proxy implements Subject {
-    private Subject subject;
+    private Subject subject;    //桥接模式这儿放的是接口
+    private SubjectImpl subjectImpl;    //代理模式这儿放的是具体实现类
 
     public Proxy(Subject subject) {
         this.subject = subject;
     }
+    public Proxy(SubjectImpl subjectImpl) {
+        this.subjectImpl = subjectImpl;
+    }
 
     @Override
     public void print() {
-        System.out.println("before");
+        //System.out.println("before");
         subject.print();
-        System.out.println("after");
+        //System.out.println("after");
     }
 
     public static void main(String[] args) {
-        Subject subject = new Proxy(new SubjectImpl2());
+        Subject subject = new Proxy(new SubjectImpl());//如果print方法未修改,这踏马就是个桥接模式
         subject.print();
     }
 }
