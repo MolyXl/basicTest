240e80edd800ad169d13cb4b4004dc2816aa4304
MolyXl <345957335@qq.com>
2019-06-27 09:14:16 +0800

    设计模式,观察者模式

--- src/pattern/template/AbstractCalculator.java
+++ src/pattern/template/AbstractCalculator.java
@@ -5,6 +5,8 @@ package pattern.template;
  * @description 抽象类, 提供辅助函数,非必须
  * @date 2019/6/26
  */
+//模板方法模式：一个抽象类中，有一个主方法，再定义1…n个方法，可以是抽象的，
+// 也可以是实际的方法，定义一个类，继承该抽象类，重写抽象方法，通过调用抽象类，实现对子类的调用
 public abstract class AbstractCalculator {
     public int[] split(String exp, String opt) {
         String array[] = exp.split(opt);
