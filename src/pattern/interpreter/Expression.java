e46337a839e05bef6102a60487a8002173aff5a9
MolyXl <345957335@qq.com>
2019-06-27 15:52:43 +0800

    设计模式,解释器模式

---
+++ src/pattern/interpreter/Expression.java
@@ -0,0 +1,16 @@
+package pattern.interpreter;
+
+/**
+ * @author MaZhuli
+ * @description
+ * @date 2019/6/27
+ */
+public interface Expression {
+    public int interpret(Context context);
+
+    public static void main(String[] args) {
+        int result = new Minus().interpret((new Context(new Plus()
+                .interpret(new Context(9, 2)), 8)));
+        System.out.println(result);
+    }
+}
---
+++ src/pattern/interpreter/Plus.java
@@ -0,0 +1,13 @@
+package pattern.interpreter;
+
+/**
+ * @author MaZhuli
+ * @description
+ * @date 2019/6/27
+ */
+public class Plus implements Expression {
+    @Override
+    public int interpret(Context context) {
+        return context.getNum1()+context.getNum2();
+    }
+}
