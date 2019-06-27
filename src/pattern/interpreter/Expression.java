14cae697ae7424c112aeee2278f3891fcecd1a17
MolyXl <345957335@qq.com>
2019-06-27 16:18:19 +0800

    设计模式,解释器模式

--- src/pattern/interpreter/Expression.java
+++ src/pattern/interpreter/Expression.java
@@ -5,12 +5,12 @@ package pattern.interpreter;
  * @description
  * @date 2019/6/27
  */
+//解释着模式
 public interface Expression {
     public int interpret(Context context);
 
     public static void main(String[] args) {
-        int result = new Minus().interpret((new Context(new Plus()
-                .interpret(new Context(9, 2)), 8)));
+        int result = new Minus().interpret((new Context(new Plus().interpret(new Context(9, 2)), 8)));
         System.out.println(result);
     }
 }
