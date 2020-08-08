3bce078054ce99bdea7e693c8c059d8345646834
MaZhuli <345957335@qq.com>
2020-08-08 11:09:48 +0800

    exception

--- src/exception/TryCatchFinally.java
+++ src/exception/TryCatchFinally.java
@@ -5,14 +5,16 @@ public class TryCatchFinally {
         int test = test();
         System.out.println(test);
     }
-    public static int test(){
+
+    public static int test() {
+        //finally返回值为最终返回值,finally中修改不影响try返回值
         int i = 1;
         try {
-            System.out.println(1/0);
+            System.out.println(1 / 0);
             return ++i;
-        }catch (Exception e){
+        } catch (Exception e) {
             return 1;
-        }finally {
+        } finally {
             ++i;
         }
     }
